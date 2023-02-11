5d8af03e2c0144786c1b35082c86e84c0f3cdbff
Himanshu <himanshuyadav711@gmail.com>
2023-02-11 11:47:19 +0530

    made changes accourding to pr comments

--- Week-3/3.1/Memorise.js
+++ Week-3/3.1/Memorise.js
@@ -1,30 +1,24 @@
 function memorise(fn) {
-    const cache = new Map();
-    return function (...args) {
-        const key = args.toString();
-        if (cache.has(key)) {
-            return cache.get(key);
-        }
+  const cache = new Map();
+  return function (...args) {
+    const key = args.toString();
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
 
-        cache.set(key, fn(...args));
-        return cache.get(key);
-
-
-
-
-    };
+    cache.set(key, fn(...args));
+    return cache.get(key);
+  };
 }
 
 function sum(...args) {
-    return args.reduce(function (acc, curr) {
-        return acc + curr;
-    }, 0);
+  return args.reduce(function (acc, curr) {
+    return acc + curr;
+  }, 0);
 }
-
 
 const memoriseAdd = memorise(sum);
 
 console.log(memoriseAdd(10)); // => 10
-console.log(memoriseAdd(10, 100, 7)); // => 117 
+console.log(memoriseAdd(10, 100, 7)); // => 117
 console.log(memoriseAdd(10, 100)); // => 110
-
